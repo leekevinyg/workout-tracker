b615dbedb27aa4b9093171bb55504e11a72be88f
klee2010 <klee2010@gmail.com>
2018-12-23 00:21:37 -0800

    added an accent color to the exercise card

--- src/components/ExerciseCard/styled/WrapperSC.js
+++ src/components/ExerciseCard/styled/WrapperSC.js
@@ -3,21 +3,18 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-
   padding: 20px;
+  border-top: 6px solid red;
   box-shadow: 5px 5px 30px rgba(0,0,0,0.2);
-  border-radius: 10px;
+  border-radius: 0 0 3px 3px;
   font: 'Hind', sans-serif;
-
   width: 300px;
   height: 300px;
-
   position: absolute;
   top:0;
   bottom: 0;
   left: 0;
   right: 0;
-
   margin: auto;
 `;
 
