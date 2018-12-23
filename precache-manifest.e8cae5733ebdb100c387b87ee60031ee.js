af5f2f379649f21f54670976ac49a27c974cf060
klee2010 <klee2010@gmail.com>
2018-12-22 23:39:06 -0800

    Updates

---
+++ precache-manifest.e8cae5733ebdb100c387b87ee60031ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b88e5ada4fb4ed155d56",
+    "url": "/workout-tracker/static/js/runtime~main.b88e5ada.js"
+  },
+  {
+    "revision": "d696d2e9f9fa4ca461a3",
+    "url": "/workout-tracker/static/js/main.d696d2e9.chunk.js"
+  },
+  {
+    "revision": "c507fd893e2720c647f8",
+    "url": "/workout-tracker/static/js/1.c507fd89.chunk.js"
+  },
+  {
+    "revision": "d696d2e9f9fa4ca461a3",
+    "url": "/workout-tracker/static/css/main.0ad4420a.chunk.css"
+  },
+  {
+    "revision": "31de7b6956f754050962c5b3604eec91",
+    "url": "/workout-tracker/index.html"
+  }
+];
